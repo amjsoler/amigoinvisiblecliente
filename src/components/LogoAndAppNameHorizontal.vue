262255e5232cc8a912fa6e6957447aa7560ebbac
Jorge Amat Soler <amjsoler@gmail.com>
2024-03-04 10:14:46 +0100

    resolver bug de app name

--- src/components/LogoAndAppNameHorizontal.vue
+++ src/components/LogoAndAppNameHorizontal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
     <app-logo class="w-8 h-8" />
-    {{ $t("APP_TITLE")}}
+    {{ $t("APP_NAME")}}
   </div>
 </template>
 <script>
